9a3d2cef90bda6dd765f2a8fc389d817318ba10b
DESKTOP-4EV28C1\WANHEDA <annakoshkina099@gmail.com>
2025-09-08 02:44:29 +0300

    update

--- ms-converter/cmd/Program.cs
+++ ms-converter/cmd/Program.cs
@@ -1,6 +1,15 @@
 using ms_converter.service;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddWindowsService(options => options.ServiceName = "MsConverter");
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSimpleConsole(o =>
+{
+    o.TimestampFormat = "HH:mm:ss ";
+    o.SingleLine = true;
+});
+
 builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 builder.Services.Configure<RabbitOptions>(builder.Configuration.GetSection("Rabbit"));
 builder.Services.Configure<StorageOptions>(builder.Configuration.GetSection("Storage"));
@@ -8,8 +17,8 @@ builder.Services.Configure<S3Options>(builder.Configuration.GetSection("S3"));
 
 builder.Services.AddHttpClient<Storage>();
 builder.Services.AddTransient<Converter>();
-builder.Services.AddTransient<S3Uploader>(); 
+builder.Services.AddSingleton<S3Uploader>();
 builder.Services.AddHostedService<Consumer>();
 
 var app = builder.Build();
-app.Run();
+await app.RunAsync();
